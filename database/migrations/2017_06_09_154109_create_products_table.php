8c8b676c54a689486b84cca258ae2b85e52c8103
Samir <samir.guitar@gmail.com>
2017-06-09 13:09:40 -0300

    #12 - Migrations

---
+++ database/migrations/2017_06_09_154109_create_products_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function(Blueprint $table){
+          $table->increments('id');
+          $table->string('name',150);
+          $table->integer('number');
+          $table->boolean('active');
+          $table->string('image',20);
+          $table->enum('category',['eletronics','moveis', 'limpeza','banho']);
+          $table->text('description');
+          $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('products');
+
+//          $table->increments('id');
+//          $table->string('name',150);
+//          $table->integer('number');
+//          $table->boolean('active');
+//          $table->string('image',20);
+//          $table->enum('category',['eletronics','moveis', 'limpeza','banho']);
+//          $table->text('description');
+//          $table->timestamp();
+    }
+}
