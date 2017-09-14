4c8f529e9d6eebef5f7a9912906ea8bd4b8d2e07
Samir <samir.guitar@gmail.com>
2017-09-14 13:29:32 -0300

    #27 Update dados

--- app/Http/Controllers/Painel/ProdutoController.php
+++ app/Http/Controllers/Painel/ProdutoController.php
@@ -135,7 +135,7 @@ class ProdutoController extends Controller
     $title = "Editar Produto: {$product->name}";
     $categorys = ['eletronics', 'moveis', 'limpeza', 'banho'];
 
-    return view('painel.products.create-edit', compact('title','categorys', 'product'));
+    return view('painel.products.create-edit', compact('title', 'categorys', 'product'));
 
   }
 
@@ -147,9 +147,28 @@ class ProdutoController extends Controller
    * @param  int $id
    * @return \Illuminate\Http\Response
    */
-  public function update(Request $request, $id)
+  public function update(ProductFormRequest $request, $id)
   {
-    return "Editando o item de id {$id}";
+//    return "Editando o item de id {$id}";
+
+    //Recupera todos os dados do formulário
+    $dataForm = $request->all();
+
+    //recupera o item para editar
+    $product = $this->product->find($id);
+
+    // Verifica se o produto está ativado
+    $dataForm['active'] = (!isset($dataForm['active'])) ? 0 : 1;
+
+    // Altera os itens
+    $update = $product->update($dataForm);
+
+    //Verifica se realmente editou
+    if ($update)
+      return redirect()->route('produtos.index');
+    else
+      return redirect()->route('produtos.edit', $id)->with(['errors' => 'Falha ao editar']);
+
   }
 
   /**
